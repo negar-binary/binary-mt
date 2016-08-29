90fb5776886a71065e52890bc213dbd8fc74843f
negar <negar@binary.com>
2016-08-29 14:20:39 +0800

    Auto-generated commit

--- js/binary.js
+++ js/binary.js
@@ -27098,6 +27098,31 @@ for (var key in texts_json) {
 
 })();
 
+;function format_money(currency, amount) {
+    var symbol = format_money.map[currency];
+    if (symbol === undefined) {
+        return currency + ' ' + amount;
+    }
+    return symbol + amount;
+}
+
+// Taken with modifications from:
+//    https://github.com/bengourley/currency-symbol-map/blob/master/map.js
+// When we need to handle more currencies please look there.
+format_money.map = {
+    "USD": "$",
+    "GBP": "£",
+    "AUD": "A$",
+    "EUR": "€",
+    "JPY": "¥",
+};
+
+if (typeof module !== 'undefined') {
+    module.exports = {
+        format_money: format_money,
+    };
+}
+
 ;function check_login_hide_signup() {
     if (page.client.is_logged_in) {
         $('#verify-email-form').remove();
